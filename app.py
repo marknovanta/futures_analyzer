650e9d701a2798cbdf720ff4624ff697274c87c3
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-04-11 18:01:49 +0200

    Calc percentage of move range

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import pandas as pd
-from helper import print_info, tick_round, get_hist
+from helper import print_info, tick_round, get_hist, calc_ranges
 
 future = yf.Ticker('ES=F')
 
@@ -9,14 +9,19 @@ data = get_hist(future)
 print()
 print(data)
 
+# DATA EXTRACT
 avg_range = data['Range'].mean()
 avg_vol = data['Volume'].mean()
 avg_up_move = data['Open to Hi'].mean()
 avg_dwn_move = data['Open to Lo'].mean()
 
 days = len(data)
+
 max_up = data['Open to Hi'].max()
 max_dwn = data['Open to Lo'].max()
+
+ranges_up = calc_ranges(data, 'Hi')
+ranges_dwn = calc_ranges(data, 'Lo')
 
 
 print()
@@ -29,4 +34,13 @@ print(f'AVG Lower move from open: {tick_round(avg_dwn_move)}')
 print()
 print(f'Max up move from open: {max_up}')
 print(f'Max down move from open: {max_dwn}')
+
+print('\nMOVES FROM OPEN TO HIGH:')
+for key, value in ranges_up.items():
+    print(f'{key}: {round((value/days)*100, 2)}%')
+
+print('\nMOVES FROM OPEN TO LOW:')
+for key, value in ranges_dwn.items():
+    print(f'{key}: {round((value/days)*100, 2)}%')
+
 print()
--- helper.py
+++ helper.py
@@ -19,3 +19,45 @@ def get_hist(ticker):
     hist['Open to Lo'] = hist['Open'] - hist['Low']
     #hist['Volume'] = hist.apply(lambda x: '{:,}'.format(round(x['Volume'])), axis=1)
     return hist
+
+def calc_ranges(data, direction):
+    # define ranges
+    # 0-5 | 5-10 | 10-15 | 15-20 | 20-25 | 25-30 | 30-35 | 35-40 | 40-45 | 45-50 | 50+
+    ranges = {
+        '0-5': 0,
+        '5-10': 0,
+        '10-15': 0,
+        '15-20': 0,
+        '20-25': 0,
+        '25-30': 0,
+        '30-35': 0,
+        '35-40': 0,
+        '40-45': 0,
+        '45-50': 0,
+        '50+': 0,
+    }
+    for r in data[f'Open to {direction}']:
+        if r > 0 and r <= 5:
+            ranges['0-5'] += 1
+        elif r > 5 and r <= 10:
+            ranges['5-10'] += 1
+        elif r > 10 and r <= 15:
+            ranges['10-15'] += 1
+        elif r > 15 and r <= 20:
+            ranges['15-20'] += 1
+        elif r > 20 and r <= 25:
+            ranges['20-25'] += 1
+        elif r > 25 and r <= 30:
+            ranges['25-30'] += 1
+        elif r > 30 and r <= 35:
+            ranges['30-35'] += 1
+        elif r > 35 and r <= 40:
+            ranges['35-40'] += 1
+        elif r > 40 and r <= 45:
+            ranges['40-45'] += 1
+        elif r > 45 and r <= 50:
+            ranges['45-50'] += 1
+        elif r > 50:
+            ranges['50+'] += 1
+
+    return ranges
