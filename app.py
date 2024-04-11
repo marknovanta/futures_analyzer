14b6861bf4fa6d6e6489a77e93aac94d583492eb
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-04-11 17:39:37 +0200

    Calc max moves up and down from open

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ avg_vol = data['Volume'].mean()
 avg_up_move = data['Open to Hi'].mean()
 avg_dwn_move = data['Open to Lo'].mean()
 
+days = len(data)
+max_up = data['Open to Hi'].max()
+max_dwn = data['Open to Lo'].max()
+
+
 print()
 # open is at midnight
 
@@ -21,4 +26,7 @@ print(f'AVG Range: {tick_round(avg_range)}')
 print(f'AVG Volume: {round(avg_vol):,}')
 print(f'AVG Upper move from open: {tick_round(avg_up_move)}')
 print(f'AVG Lower move from open: {tick_round(avg_dwn_move)}')
+print()
+print(f'Max up move from open: {max_up}')
+print(f'Max down move from open: {max_dwn}')
 print()
