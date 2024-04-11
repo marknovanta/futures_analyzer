0d46f15b8d54c508f9570179f144f23a68ff8670
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-04-11 16:18:07 +0200

    Change from 1mo to all available data

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ def print_info(ticker):
         print(f'{key}: {value}')
 
 def get_hist(ticker):
-    # get last month data
-    hist = ticker.history(period='1mo')
+    # get all available data
+    hist = ticker.history(period='max')
     hist.drop(hist.columns[[-1,-2]], axis=1, inplace=True)
     hist['Range'] = round(hist['High'] - hist['Low'], 2)
     hist['Low'] = round(hist['Low'], 2)
@@ -26,7 +26,6 @@ avg_range = data['Range'].mean()
 avg_vol = data['Volume'].mean()
 
 print()
-print('LAST MONTH')
 print(f'AVG Range: {round(avg_range, 2)}')
 print(f'AVG Volume: {round(avg_vol):,}')
 print()
